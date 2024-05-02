9fdc51b2b1af1d7ffaef1ae3425ec0d7ce38a5fb
Milosz Piechota <milpie51@gmail.com>
2024-05-02 16:07:21 +0200

    Added Dockerfile1 with nginx

--- Simpleweb/index.js
+++ Simpleweb/index.js
@@ -1,35 +1,34 @@
-
 const express = require('express');
 const os = require('os');
-
 
 const app = express();
 const port = 8080;
 
 // Definicja ścieżki głównej ("/") 
 app.get('/', (req, res) => {
-  let response = `Adres IP serwera: ${getIPAddress()}\n`;
-  response += `Nazwa serwera (hostname): ${os.hostname()}\n`;
-  response += `Wersja aplikacji: ${process.env.VERSION}\n`;
-  // wyslij do klineta
-  res.send(response);
+    let response = `Adres IP serwera: ${getIPAddress()}\n`;
+    response += `Nazwa serwera (hostname): ${os.hostname()}\n`;
+    response += `Wersja aplikacji: ${process.env.VERSION}\n`;
+    // Wysyłamy odpowiedź jako HTML
+    res.send(response);
 });
 
+// Serwowanie pliku HTML
+app.use(express.static('public'));
 
 app.listen(port, () => {
-  console.log(`Aplikacja jest dostępna na porcie ${port}`);
+    console.log(`Aplikacja jest dostępna na porcie ${port}`);
 });
 
-
 function getIPAddress() {
-  // pobierz interfejsy sieciowe
-  const interfaces = os.networkInterfaces();
-  for (const dev in interfaces) {
-    // filtracja adresów IPv4, wykluczając adresy wewnętrzne
-    const iface = interfaces[dev].filter(details => details.family === 'IPv4' && !details.internal);
-   // zwróć pierwszy z nich
-    if (iface.length > 0) return iface[0].address;
-  }
-// zwróć wartość domyślną
-  return '0.0.0.0';
+    // Pobierz interfejsy sieciowe
+    const interfaces = os.networkInterfaces();
+    for (const dev in interfaces) {
+        // Filtruj adresy IPv4, wykluczając adresy wewnętrzne
+        const iface = interfaces[dev].filter(details => details.family === 'IPv4' && !details.internal);
+        // Zwróć pierwszy znaleziony adres
+        if (iface.length > 0) return iface[0].address;
+    }
+    // Zwróć wartość domyślną
+    return '0.0.0.0';
 }
